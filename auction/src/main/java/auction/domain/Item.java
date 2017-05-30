8299f1b830e25362e5b2e2c3d18d703b914a08c6
Pigna <myron.antonissen@gmail.com>
2017-05-30 14:28:31 +0200

    removed id from hash

--- auction/src/main/java/auction/domain/Item.java
+++ auction/src/main/java/auction/domain/Item.java
@@ -77,14 +77,13 @@ public class Item implements Serializable, Comparable {
         return false;
     }
 
-    // zal we lniet werken. Mvg Sibe
     @Override
     public int hashCode() 
     {
         final int prime = 31;
         int result = 1;
         result = prime * result +((description == null) ? 0 : description.hashCode());
-        result = prime * result + id.intValue();
+        //result = prime * result + id.intValue();
         result = prime * result + ((category == null) ? 0 : category.hashCode());
         result = prime * result + ((seller == null) ? 0 : seller.hashCode());
         result = prime * result + ((highest == null) ? 0 : highest.hashCode());
