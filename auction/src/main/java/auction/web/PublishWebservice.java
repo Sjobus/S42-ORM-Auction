38b8a500456e05e4302fdeb3431a04fed432695d
lestuckair <k.mangre@student.fontys.nl>
2017-06-05 16:45:50 +0200

    Web url change

---
+++ auction/src/main/java/auction/web/PublishWebservice.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auction.web;
+
+import javax.xml.ws.Endpoint;
+
+/**
+ *
+ * @author Kevin
+ */
+public class PublishWebservice {
+    
+    private static final String url = "http://localhost:6080/WebAuction";
+    private static final String url2 = "http://localhost:6080/WebRegistration";
+    
+    public static void main(String[] args){
+        Endpoint.publish(url, new Auction());
+        Endpoint.publish(url2, new Auction());
+    }
+}
