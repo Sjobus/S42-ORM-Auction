4d2a044b504825c28f0321699f71d0c3e94f3d25
lestuckair <k.mangre@student.fontys.nl>
2017-06-05 17:41:38 +0200

    methodes in client

--- action_SOAP_Client/src/action_client/Action_Client.java
+++ action_SOAP_Client/src/action_client/Action_Client.java
@@ -5,6 +5,10 @@
  */
 package action_client;
 
+import auction.web.Bid;
+import auction.web.Item;
+import auction.web.User;
+
 /**
  *
  * @author myron
@@ -17,5 +21,48 @@ public class Action_Client {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
+    private static java.util.List<auction.web.Item> findItemByDescription(java.lang.String arg0) {
+        auction.web.AuctionService service = new auction.web.AuctionService();
+        auction.web.Auction port = service.getAuctionPort();
+        return port.findItemByDescription(arg0);
+    }
+
+    private static Item getItem(java.lang.Long arg0) {
+        auction.web.AuctionService service = new auction.web.AuctionService();
+        auction.web.Auction port = service.getAuctionPort();
+        return port.getItem(arg0);
+    }
+
+    private static Bid newBid(auction.web.Item arg0, auction.web.User arg1, auction.web.Money arg2) {
+        auction.web.AuctionService service = new auction.web.AuctionService();
+        auction.web.Auction port = service.getAuctionPort();
+        return port.newBid(arg0, arg1, arg2);
+    }
+
+    private static Item offerItem(auction.web.User arg0, auction.web.Category arg1, java.lang.String arg2) {
+        auction.web.AuctionService service = new auction.web.AuctionService();
+        auction.web.Auction port = service.getAuctionPort();
+        return port.offerItem(arg0, arg1, arg2);
+    }
+
+    private static boolean revokeItem(auction.web.Item arg0) {
+        auction.web.AuctionService service = new auction.web.AuctionService();
+        auction.web.Auction port = service.getAuctionPort();
+        return port.revokeItem(arg0);
+    }
+
+    private static User getUser(java.lang.String arg0) {
+        auction.web.RegistrationService service = new auction.web.RegistrationService();
+        auction.web.Registration port = service.getRegistrationPort();
+        return port.getUser(arg0);
+    }
+
+    private static User registerUser(java.lang.String arg0) {
+        auction.web.RegistrationService service = new auction.web.RegistrationService();
+        auction.web.Registration port = service.getRegistrationPort();
+        return port.registerUser(arg0);
+    }
+    
     
 }
